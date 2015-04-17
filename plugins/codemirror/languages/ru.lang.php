7dfa5dd932ea284f98eccf19ac80eb84ed6e7fd1
promo360 <promo360@yandex.ru>
2015-04-17 18:58:47 +0500

    Исправление в описании codemirror

--- plugins/codemirror/languages/ru.lang.php
+++ plugins/codemirror/languages/ru.lang.php
@@ -3,6 +3,6 @@
     return array(
         'codemirror' => array(
             'CodeMirror' => 'CodeMirror',
-            'CodeMirror - это JavaScript библиотека для подсветки синтаксиса в редакторе кода',
+            'CodeMirror is a versatile text editor implemented in JavaScript for the browser.' => 'CodeMirror - это JavaScript библиотека для подсветки синтаксиса в редакторе кода',
         ) 
     );
